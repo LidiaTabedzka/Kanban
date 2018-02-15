78ab5dc266ecbf6b63340902b374a219da36a273
Lidia Tabędzka <lidia.tabedzka@gmail.com>
2018-02-15 17:54:22 +0100

    Add drag&drop function

--- client/modules/Lane/LaneContainer.js
+++ client/modules/Lane/LaneContainer.js
@@ -1,7 +1,10 @@
 import { connect } from 'react-redux';
 import Lane from './Lane';
-import { updateLaneRequest, editLane, deleteLaneRequest } from './LaneActions';
+import { updateLaneRequest, editLane, deleteLaneRequest, moveBetweenLanes } from './LaneActions';
 import { createNoteRequest } from '../Note/NoteActions';
+import { compose } from 'redux';
+import { DropTarget } from 'react-dnd';
+import ItemTypes from '../Kanban/itemTypes';
 
 const mapStateToProps = (state, ownProps) => {
     return {
@@ -14,6 +17,25 @@ const mapDispatchToProps = {
     deleteLane: deleteLaneRequest,
     updateLane: updateLaneRequest,
     addNote: createNoteRequest,
+    moveBetweenLanes,
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Lane);
+const noteTarget = {
+    hover(targetProps, monitor) {
+      const sourceProps = monitor.getItem();
+      const { id: noteId, laneId: sourceLaneId } = sourceProps;
+
+      if (!targetProps.lane.notes.length) {targetProps.moveBetweenLanes(
+        targetProps.lane.id,
+        noteId,
+        sourceLaneId,
+      )};
+    },
+};
+
+export default compose(
+    connect(mapStateToProps, mapDispatchToProps),
+    DropTarget(ItemTypes.NOTE, noteTarget, (dragConnect) => ({
+      connectDropTarget: dragConnect.dropTarget()
+    }))
+  )(Lane);
